a2469ed45c15a9b20bbf509864834c01cd4e4e1d
Faron-Pan <1937286231@qq.com>
2023-08-07 01:29:49 +0800

    a

--- files/script.js
+++ files/script.js
@@ -1,21 +1,25 @@
-window.onload = function() {  
-    var systemThemeColor = window.getComputedStyle(document.body).getPropertyValue('--theme-color');  
-    var body = document.body;  
-    
-    if (isDarkColor(systemThemeColor)) {  
-      body.classList.add("dark-mode");  
-    } else {  
-      body.classList.add("light-mode");  
-    }  
-    var imageElement = document.getElementById('myImage');  
-    
-    // 根据系统主题选择不同的图片  
-    if (isDarkColor(systemThemeColor)) {  
-      imageElement.src = "./files/dark_image.png";  
-    } else {  
-      imageElement.src = "./files/light_image.jpeg";
-    } 
-  };  
+function getSystemTheme() {  
+  if (typeof window.matchMedia === "function") {  
+    var mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");  
+    return mediaQuery.active ? mediaQuery.media.mimeType : "unknown";  
+  } else {  
+    return "unknown";  
+  }  
+}  
+  
+var theme = getSystemTheme();  
+if (theme === "text/css") {  
+  document.body.classList.add("dark-mode");  
+} else {  
+  document.body.classList.add("light-mode");  
+} 
+var imageElement = document.getElementById('myImage');  
+// 根据系统主题选择不同的图片  
+if (isDarkColor(systemThemeColor)) {  
+  imageElement.src = "./files/dark_image.png";  
+} else {  
+  imageElement.src = "./files/light_image.jpeg";
+}   
     
   function isDarkColor(color) {  
     var rgb = parseInt(color.substring(5), 16);  
